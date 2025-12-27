6abe8ac2c85c739bc91d8e875c00078f95b18c36
Capplequoppe <contact@hallgrenstudios.com>
2025-12-27 22:09:56 +0100

    trigger release

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 /**
  * Components Layer
  *
- * React components for the CharaChorder Trainer application.
+ * React components for the CharaChorder  Trainer application.
  *
  * @module components
  */
